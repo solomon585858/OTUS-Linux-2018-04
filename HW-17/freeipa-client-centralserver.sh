ea79447ec1153c65fbe137dc2f9553f9d2378104
Marat Akhmetianov <marat.akhmetianov@gmail.com>
2018-07-10 17:59:37 +0300

    added files for HW-17

---
+++ HW-17/freeipa-client-centralserver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+hostname centralserver.otus.local
+> /etc/resolv.conf && echo "search otus.local" >> /etc/resolv.conf && echo "nameserver 192.168.0.3" >> /etc/resolv.conf
+chattr +i /etc/resolv.conf
+> /etc/hostname && echo "centralserver.otus.local" >> /etc/hostname
+
+yum update -y
+
+yum install bind-utils -y
+  if [ $? -eq 0 ]; then
+    echo "Command executed successfully!"
+  else
+    echo "Command install bind-utils completed with error. FAIL!!!"
+    exit 1
+fi
+
+systemctl restart network
+
+yum install freeipa-client -y
+  if [ $? -eq 0 ]; then
+    echo "Command executed successfully!"
+  else
+    echo "Command install freeipa-client completed with error. FAIL!!!"
+    exit 1
+fi
+
+ipa-client-install --principal admin --password Password -U --mkhomedir
